bcf33e143a6082dcd064220e8f23fae5933354cd
oon arfiandwi <oon.arfiandwi@gmail.com>
2018-09-24 00:00:58 +0800

    tugas leaflet ke-2 untuk menggabungkan dengan CSS Grid. ulasan Quiz MWS 2.

---
+++ public/footer-html.js
@@ -0,0 +1,9 @@
+document.write('\
+\
+<div class="footer">\
+<p>&nbsp;</p>\
+<p>&larr;<a href="//gdkmws.firebaseapp.com">Kembali ke halaman utama</a>&nbsp;</p>\
+<p>&copy;2018 oon arfiandwi, peserta Google Developer Kejar, Mobile Web Specialist, Kota Makassar, batch #1<br/>\
+<a target="_blank" href="https://github.com/oonid/gdkmws">kode sumber dari halaman web ini</a></p>\
+</div>\
+');
