ddd3215a34b67d5d067da2a710f0b10447941cef
oon arfiandwi <oon.arfiandwi@gmail.com>
2018-09-24 00:29:21 +0800

    perbaikan BUG, input dari program tambahkan dua angka, jika dikosongkan (placeholder) tidak dianggap angka 0.

--- public/project1/add2numbers.js
+++ public/project1/add2numbers.js
@@ -1,8 +1,8 @@
 // add2numbers.js
 function kalkulator() {
     let angka = document.querySelectorAll('input');
-    let i1= angka[0].value ; //angka pertama
-    let i2= angka[1].value ; //angka kedua
+    let i1= angka[0].value || 0; //angka pertama, jika tidak diisi, digantikan angka 0
+    let i2= angka[1].value || 0; //angka kedua, jika tidak diisi, digantikan angka 0
     if(isNaN(i1) || isNaN(i2)) {
         alert('Anda harus memasukkan Angka (bulat atau pecahan).')
     }
