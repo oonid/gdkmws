30f7ea36ac0fe0dc802af466b3680447c174ced4
oon arfiandwi <oon.arfiandwi@gmail.com>
2018-10-31 20:25:50 +0800

    menambahkan sejumlah perbaikan untuk audit lagi khususnya di direktori project3, nilai audit untuk PWA masih kurang karena service workers di direktori utama tidak dikenali.

--- public/footer-html.js
+++ public/footer-html.js
@@ -4,6 +4,6 @@ document.write('\
 <p>&nbsp;</p>\
 <p>&larr;<a href="//gdkmws.firebaseapp.com">Kembali ke halaman utama</a>&nbsp;</p>\
 <p>&copy;2018 oon arfiandwi, peserta Google Developer Kejar, Mobile Web Specialist, Kota Makassar, batch #1<br/>\
-<a target="_blank" href="https://github.com/oonid/gdkmws">kode sumber dari halaman web ini</a></p>\
+<a target="_blank" href="https://github.com/oonid/gdkmws" rel="noreferrer">kode sumber dari halaman web ini</a></p>\
 </div>\
 ');
